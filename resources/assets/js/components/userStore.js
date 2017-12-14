41f256ec6bf98a2442e01131765cc1fbc2d1ce98
blubMe <fahmiirsyad10@gmail.com>
2017-12-14 08:15:00 +0700

    fixing package with eslint

--- resources/assets/js/components/userStore.js
+++ resources/assets/js/components/userStore.js
@@ -1,15 +1,15 @@
 const state = {
   authUser: null
-}
+};
 const mutations = {
   SET_AUTH_USER (state, userObj) {
-    state.authUser = userObj
+    state.authUser = userObj;
   }
-}
+};
 const actions = {
 
-}
+};
 
 export default {
   state, mutations , actions
-}
+};
