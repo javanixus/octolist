82b823cb9591f67e2f1a7c588fbb423a117dc833
Harpi <justharpi@gmail.com>
2017-12-30 00:01:39 +0700

    all about database

---
+++ database/migrations/2017_12_29_133159_create_cards_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCardsMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('cards_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_students')->unsigned();
+            $table->integer('id_cards')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('cards_members');
+    }
+}
---
+++ database/migrations/2017_12_29_133227_create_project_members_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateProjectMembersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('project_members', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('id_projects');
+            $table->integer('id_students');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('project_members');
+    }
+}
