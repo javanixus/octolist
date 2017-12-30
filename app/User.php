968e259e63c5e77aa8f0477c87a07c8dc683b950
Harpi <justharpi@gmail.com>
2017-12-30 16:59:46 +0700

    insert user/siswa

--- app/User.php
+++ app/User.php
@@ -10,7 +10,7 @@ class User extends Authenticatable
     use Notifiable;
 
     protected $fillable = [
-        'name', 'username', 'email', 'password',
+        'name', 'username', 'email', 'password', 'nis', 'bio', 'gender', 'phone', 'id_class', 'id_major'
     ];
 
     protected $hidden = [
