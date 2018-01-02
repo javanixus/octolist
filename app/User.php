151ea3b3a925264147e60c947b718bcb92dc3ae6
Harpi <justharpi@gmail.com>
2018-01-02 21:20:57 +0700

    add mutator and acessor student

--- app/User.php
+++ app/User.php
@@ -9,6 +9,17 @@ class User extends Authenticatable
 {
     use Notifiable;
 
+    public function getNameAttribute($name){ // Accessor
+        return ucwords(strtolower($name));
+    }
+
+    public function setNameAttribute($name){ // Mutator
+        return $this->attributes['name'] = strtolower($name);
+    }
+    public function setPasswordAttribute($password){ // Mutator
+        return $this->attributes['password'] = bcrypt($password);
+    }
+
     protected $fillable = [
         'name', 'username', 'email', 'password', 'nis', 'bio', 'gender', 'phone', 'id_class', 'id_major'
     ];
--- routes/api.php
+++ routes/api.php
@@ -2,28 +2,16 @@
 
 use Illuminate\Http\Request;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
 Route::post('/login', 'UserController@login');
 
-
 Route::group(['middleware' => ['api','cors'], 'prefix' => 'v1'], function(){
     Route::post('/user/signin', 'AuthController@signin');
-    Route::post('/user/signup', 'AuthController@store');
-    Route::post('/user/create', 'UserController@store');
+//    Route::post('/user/signup', 'AuthController@store');
+    Route::post('/user', 'UserController@store');
     Route::get('/user', 'UserController@index');
     Route::patch('/user/{id}', 'UserController@update');
     Route::delete('/user/{id}', 'UserController@destroy');
@@ -32,10 +20,5 @@ Route::group(['middleware' => ['api','cors'], 'prefix' => 'v1'], function(){
 			Route::get('/dashboard','UserController@home')->name('dashboard');
 			// Route::post('/profile/edit/','TestAuthController@EditProfile');
 			Route::patch('/profile/teacher/edit/','TeacherController@update');
-		});
-});
-
-
-Route::group(['middleware' => ['api'],'prefix' => 'testing'] , function(){
-		Route::post('/signup','TestAuthController@signup');
+    });
 });
