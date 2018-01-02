9c6e5313df7b531e8ef8378f22ce269b1859882f
ppy <regakun836@gmail.com>
2018-01-02 21:26:18 +0700

    Teacher Creation

--- routes/api.php
+++ routes/api.php
@@ -19,22 +19,16 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 Route::post('/login', 'UserController@login');
 
-
 Route::group(['middleware' => ['api','cors'], 'prefix' => 'v1'], function(){
-    Route::post('/user/signin', 'AuthController@signin');
     Route::post('/user/signup', 'AuthController@store');
-    Route::post('/user/create', 'UserController@store');
-    Route::get('/user', 'UserController@index');
-    Route::patch('/user/{id}', 'UserController@update');
-    Route::delete('/user/{id}', 'UserController@destroy');
-
-    Route::group(['middleware' => ['jwt.auth','cors']], function(){
+    Route::post('/user/signin', 'AuthController@signin');
+		Route::group(['middleware' => ['jwt.auth','cors']], function(){
 			Route::get('/dashboard','UserController@home')->name('dashboard');
 			// Route::post('/profile/edit/','TestAuthController@EditProfile');
 			Route::patch('/profile/teacher/edit/','TeacherController@update');
+			Route::post('/teacher/signup','TeacherController@store');
 		});
 });
-
 
 Route::group(['middleware' => ['api'],'prefix' => 'testing'] , function(){
 		Route::post('/signup','TestAuthController@signup');
