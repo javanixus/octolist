f66f4874d5897b37a20191ac6fd28d85d040032e
Harpi <justharpi@gmail.com>
2018-01-05 22:54:12 +0700

    solved the problem while searching users

--- routes/api.php
+++ routes/api.php
@@ -9,17 +9,27 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 Route::post('/login', 'UserController@login');
 
 Route::group(['middleware' => ['api','cors'], 'prefix' => 'v1'], function(){
-    Route::post('/user/signin', 'AuthController@signin');
-    Route::post('/user/signup', 'AuthController@store');
-    Route::post('/user', 'UserController@store');
+
     Route::get('/users', 'UserController@index');
-    Route::get('/users/{q}', 'UserController@index');
+
+    Route::post('/user/signin', 'AuthController@signin');
+
+    Route::post('/user/signup', 'AuthController@store');
+
+    Route::post('/user', 'UserController@store');
+
+//    Route::get('/users/{q}', 'UserController@index');
+
     Route::patch('/user/{id}', 'UserController@update');
+
     Route::delete('/user/{id}', 'UserController@destroy');
 
     Route::group(['middleware' => ['jwt.auth','cors']], function(){
+
 			Route::get('/dashboard','UserController@home')->name('dashboard');
+
 			// Route::post('/profile/edit/','TestAuthController@EditProfile');
+
 			Route::patch('/profile/teacher/edit/','TeacherController@update');
     });
 });
