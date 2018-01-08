8d814b726bdc8cdaf448337f059f154428183743
blubMe <fahmiirsyad10@gmail.com>
2018-01-08 21:19:16 +0700

    teacher Dashboard (wip)

---
+++ resources/assets/js/components/teacher/dashboardTeacher.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+      <header>
+          <div id="headerTeacher" class="headerTeacherWrapper">
+            <div class="headerTeacherNav">
+                <div class="headerTeacherNav__Title">
+                    <h3>Teacher Dashboard</h3>
+                </div>
+                <div class="headerTeacherNav__Items">
+                    <div class="avatar avatar--s admin-avatars">
+                        <img class="avatar-img" src="/images/love.jpg"
+                        alt="" />
+                    </div>
+                </div>
+            </div>
+            <div class="headerTeacherExtend">
+                <ul>
+                    <li @click="currentView='teacher-main'" :class="{headerTeacherExtendActive: currentView === 'teacher-main'}" >Project watch</li>
+                    <li @click="currentView='teacher-profile'" :class="{headerTeacherExtendActive: currentView === 'teacher-profile'}" >Profile edit</li>
+                </ul>
+            </div>
+          </div>
+      </header>
+      <main>
+        <component :is="currentView"></component>
+      </main>
+  </div>
+</template>
+<style>
+
+</style>
+<script>
+import axios from 'axios'
+import router from './../../router'
+import store from './../../store/index'
+import teacherDashboardMain from './dashboardTeacherMain'
+import teacherDashboardProfile from './dashboardTeacherProfile'
+
+export default {
+  data() {
+      return {
+          currentView: 'teacher-main'
+      }
+  },
+  components: {
+      'teacher-main' : teacherDashboardMain,
+      'teacher-profile' : teacherDashboardProfile
+  }
+}
+</script>
