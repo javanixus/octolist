c2beefa0861ce512f98997937afb3873fc754833
blubMe <fahmiirsyad10@gmail.com>
2018-01-10 10:49:06 +0700

    teacher update small

--- resources/assets/js/components/teacher/dashboardTeacherMain.vue
+++ resources/assets/js/components/teacher/dashboardTeacherMain.vue
@@ -3,7 +3,17 @@
       <div class="studentProjectWrapper">
         <div class="studentProject">
           <div class="studentProject--item">
-            
+            <div class="studentProjectItem__Header">
+
+            </div>
+            <div class="studentProjectItem__Cover">
+              <div class="studentProjectCircleCover">
+                
+              </div>
+            </div>
+            <div class="studentProjectItem__Title">
+
+            </div>
           </div>
         </div>
       </div>
