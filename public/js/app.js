0de8065edbfe4e4054c2f7d4ac5a7f791208919b
blubMe <fahmiirsyad10@gmail.com>
2018-02-10 12:50:04 +0700

    [redesign] page 2 pending

--- public/js/app.js
+++ public/js/app.js
@@ -45897,6 +45897,89 @@ Object.defineProperty(__webpack_exports__, "__esModule", { value: true });
 //
 //
 //
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
+//
 
 /* harmony default export */ __webpack_exports__["default"] = ({});
 
@@ -45908,7 +45991,573 @@ var render = function() {
   var _vm = this
   var _h = _vm.$createElement
   var _c = _vm._self._c || _h
-  return _c("div", { attrs: { id: "getStarted" } })
+  return _c("div", { attrs: { id: "getStarted" } }, [
+    _c(
+      "svg",
+      {
+        attrs: {
+          width: "354px",
+          height: "282px",
+          viewBox: "0 0 254 182",
+          version: "1.1",
+          xmlns: "http://www.w3.org/2000/svg",
+          "xmlns:xlink": "http://www.w3.org/1999/xlink"
+        }
+      },
+      [
+        _c("title", [_vm._v("Group")]),
+        _vm._v(" "),
+        _c("desc", [_vm._v("Created with Sketch.")]),
+        _vm._v(" "),
+        _c(
+          "defs",
+          [
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "31.3428566%",
+                  y1: "-122.977405%",
+                  x2: "56.157051%",
+                  y2: "64.292202%",
+                  id: "linearGradient-1"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#E8F1F5", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#C1D3DC", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "50%",
+                  y1: "0%",
+                  x2: "50%",
+                  y2: "100%",
+                  id: "linearGradient-2"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#E8F1F5", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#C1D3DC", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "20.4806062%",
+                  y1: "34.4148368%",
+                  x2: "128.57734%",
+                  y2: "34.4148368%",
+                  id: "linearGradient-3"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#FFDF77", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#FFF0AE", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "50%",
+                  y1: "0%",
+                  x2: "50%",
+                  y2: "44.429532%",
+                  id: "linearGradient-4"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#FE8DBE", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#CC759B", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c("rect", {
+              attrs: {
+                id: "path-5",
+                x: "93.0341513",
+                y: "41.2977782",
+                width: "56",
+                height: "66",
+                rx: "2"
+              }
+            }),
+            _vm._v(" "),
+            _c(
+              "filter",
+              {
+                attrs: {
+                  x: "-17.0%",
+                  y: "-18.2%",
+                  width: "135.7%",
+                  height: "130.3%",
+                  filterUnits: "objectBoundingBox",
+                  id: "filter-6"
+                }
+              },
+              [
+                _c("feMorphology", {
+                  attrs: {
+                    radius: "1",
+                    operator: "dilate",
+                    in: "SourceAlpha",
+                    result: "shadowSpreadOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feOffset", {
+                  attrs: {
+                    dx: "0.5",
+                    dy: "-2",
+                    in: "shadowSpreadOuter1",
+                    result: "shadowOffsetOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feGaussianBlur", {
+                  attrs: {
+                    stdDeviation: "2",
+                    in: "shadowOffsetOuter1",
+                    result: "shadowBlurOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feColorMatrix", {
+                  attrs: {
+                    values:
+                      "0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0298913043 0",
+                    type: "matrix",
+                    in: "shadowBlurOuter1"
+                  }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c("rect", {
+              attrs: {
+                id: "path-7",
+                x: "105.41201",
+                y: "47.430096",
+                width: "40.3280694",
+                height: "8.25791234",
+                rx: "1"
+              }
+            }),
+            _vm._v(" "),
+            _c(
+              "filter",
+              {
+                attrs: {
+                  x: "-23.6%",
+                  y: "-145.3%",
+                  width: "149.6%",
+                  height: "342.2%",
+                  filterUnits: "objectBoundingBox",
+                  id: "filter-8"
+                }
+              },
+              [
+                _c("feMorphology", {
+                  attrs: {
+                    radius: "1",
+                    operator: "dilate",
+                    in: "SourceAlpha",
+                    result: "shadowSpreadOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feOffset", {
+                  attrs: {
+                    dx: "0.5",
+                    dy: "-2",
+                    in: "shadowSpreadOuter1",
+                    result: "shadowOffsetOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feGaussianBlur", {
+                  attrs: {
+                    stdDeviation: "2",
+                    in: "shadowOffsetOuter1",
+                    result: "shadowBlurOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feColorMatrix", {
+                  attrs: {
+                    values:
+                      "0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0298913043 0",
+                    type: "matrix",
+                    in: "shadowBlurOuter1"
+                  }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c("rect", {
+              attrs: {
+                id: "path-9",
+                x: "108.230208",
+                y: "48.8268753",
+                width: "17.8871077",
+                height: "2.14545572",
+                rx: "1"
+              }
+            }),
+            _vm._v(" "),
+            _c(
+              "filter",
+              {
+                attrs: {
+                  x: "-53.1%",
+                  y: "-559.3%",
+                  width: "211.8%",
+                  height: "1032.2%",
+                  filterUnits: "objectBoundingBox",
+                  id: "filter-10"
+                }
+              },
+              [
+                _c("feMorphology", {
+                  attrs: {
+                    radius: "1",
+                    operator: "dilate",
+                    in: "SourceAlpha",
+                    result: "shadowSpreadOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feOffset", {
+                  attrs: {
+                    dx: "0.5",
+                    dy: "-2",
+                    in: "shadowSpreadOuter1",
+                    result: "shadowOffsetOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feGaussianBlur", {
+                  attrs: {
+                    stdDeviation: "2",
+                    in: "shadowOffsetOuter1",
+                    result: "shadowBlurOuter1"
+                  }
+                }),
+                _vm._v(" "),
+                _c("feColorMatrix", {
+                  attrs: {
+                    values:
+                      "0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.0298913043 0",
+                    type: "matrix",
+                    in: "shadowBlurOuter1"
+                  }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "50%",
+                  y1: "3.11902105%",
+                  x2: "44.6057014%",
+                  y2: "71.5643288%",
+                  id: "linearGradient-11"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#BACCD6", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#DCEBF4", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "69.9464829%",
+                  y1: "14.4677703%",
+                  x2: "46.1408899%",
+                  y2: "84.7376061%",
+                  id: "linearGradient-12"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#F0F4F7", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#DCEBF4", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "46.2326407%",
+                  y1: "12.6973365%",
+                  x2: "46.2326417%",
+                  y2: "89.5310013%",
+                  id: "linearGradient-13"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#BACCD6", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#DBEBF4", offset: "100%" }
+                })
+              ],
+              1
+            ),
+            _vm._v(" "),
+            _c(
+              "linearGradient",
+              {
+                attrs: {
+                  x1: "39.7514718%",
+                  y1: "13.9444018%",
+                  x2: "70.3353586%",
+                  y2: "75.9113821%",
+                  id: "linearGradient-14"
+                }
+              },
+              [
+                _c("stop", {
+                  attrs: { "stop-color": "#ECF3F7", offset: "0%" }
+                }),
+                _vm._v(" "),
+                _c("stop", {
+                  attrs: { "stop-color": "#D4E3EC", offset: "100%" }
+                })
+              ],
+              1
+            )
+          ],
+          1
+        ),
+        _vm._v(" "),
+        _c(
+          "g",
+          {
+            attrs: {
+              id: "Page-1",
+              stroke: "none",
+              "stroke-width": "1",
+              fill: "none",
+              "fill-rule": "evenodd"
+            }
+          },
+          [
+            _c("g", { attrs: { id: "Group" } }, [
+              _c("polygon", {
+                attrs: {
+                  id: "Path",
+                  fill: "url(#linearGradient-1)",
+                  points:
+                    "30.5525977 38.6346778 111.675711 9.2897167 221.467241 36.3486219 139.619775 65.6472244"
+                }
+              }),
+              _vm._v(" "),
+              _c("polygon", {
+                attrs: {
+                  id: "Path-6",
+                  fill: "url(#linearGradient-2)",
+                  points:
+                    "111.670364 9.28457642 112.197625 58.8088879 139.77535 65.6097084 221.37123 36.3089934"
+                }
+              }),
+              _vm._v(" "),
+              _c("rect", {
+                attrs: {
+                  id: "Rectangle",
+                  fill: "url(#linearGradient-3)",
+                  transform:
+                    "translate(207.551914, 57.242232) rotate(14.000000) translate(-207.551914, -57.242232) ",
+                  x: "199.551914",
+                  y: "0.242231563",
+                  width: "16",
+                  height: "114",
+                  rx: "2"
+                }
+              }),
+              _vm._v(" "),
+              _c(
+                "g",
+                {
+                  attrs: {
+                    id: "Rectangle-2",
+                    transform:
+                      "translate(121.034151, 74.297778) rotate(13.000000) translate(-121.034151, -74.297778) "
+                  }
+                },
+                [
+                  _c("use", {
+                    attrs: {
+                      fill: "black",
+                      "fill-opacity": "1",
+                      filter: "url(#filter-6)",
+                      "xlink:href": "#path-5"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c("use", {
+                    attrs: {
+                      fill: "url(#linearGradient-4)",
+                      "fill-rule": "evenodd",
+                      "xlink:href": "#path-5"
+                    }
+                  })
+                ]
+              ),
+              _vm._v(" "),
+              _c(
+                "g",
+                {
+                  attrs: {
+                    id: "Rectangle-2",
+                    transform:
+                      "translate(125.576045, 51.559052) rotate(13.000000) translate(-125.576045, -51.559052) "
+                  }
+                },
+                [
+                  _c("use", {
+                    attrs: {
+                      fill: "black",
+                      "fill-opacity": "1",
+                      filter: "url(#filter-8)",
+                      "xlink:href": "#path-7"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c("use", {
+                    attrs: {
+                      fill: "#F2F2F2",
+                      "fill-rule": "evenodd",
+                      "xlink:href": "#path-7"
+                    }
+                  })
+                ]
+              ),
+              _vm._v(" "),
+              _c(
+                "g",
+                {
+                  attrs: {
+                    id: "Rectangle-2",
+                    transform:
+                      "translate(117.173762, 49.899603) rotate(12.000000) translate(-117.173762, -49.899603) "
+                  }
+                },
+                [
+                  _c("use", {
+                    attrs: {
+                      fill: "black",
+                      "fill-opacity": "1",
+                      filter: "url(#filter-10)",
+                      "xlink:href": "#path-9"
+                    }
+                  }),
+                  _vm._v(" "),
+                  _c("use", {
+                    attrs: {
+                      fill: "#909090",
+                      "fill-rule": "evenodd",
+                      "xlink:href": "#path-9"
+                    }
+                  })
+                ]
+              ),
+              _vm._v(" "),
+              _c("polygon", {
+                attrs: {
+                  id: "Path-3",
+                  fill: "url(#linearGradient-11)",
+                  points:
+                    "139.751573 65.6554463 139.751573 181.092825 28.5836365 149.166728 30.5921167 38.5139688"
+                }
+              }),
+              _vm._v(" "),
+              _c("polygon", {
+                attrs: {
+                  id: "Path-4",
+                  fill: "url(#linearGradient-12)",
+                  points:
+                    "139.644705 65.6286793 108.725688 113.378912 0.577659019 83.155071 30.5455627 38.6280365"
+                }
+              }),
+              _vm._v(" "),
+              _c("polygon", {
+                attrs: {
+                  id: "Path-5",
+                  fill: "url(#linearGradient-13)",
+                  points:
+                    "139.680232 65.6253507 221.400581 36.2850184 222.39993 146.028612 139.114461 181.228381"
+                }
+              }),
+              _vm._v(" "),
+              _c("polygon", {
+                attrs: {
+                  id: "Path-2",
+                  fill: "url(#linearGradient-14)",
+                  points:
+                    "139.643051 65.6152975 166.641014 114.441045 253.454639 79.1781257 221.448096 36.3488398"
+                }
+              })
+            ])
+          ]
+        )
+      ]
+    )
+  ])
 }
 var staticRenderFns = []
 render._withStripped = true
