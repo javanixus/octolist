c43441ad65bea2b24f236b2acd7ce3d06d78670e
ppy <regakun836@gmail.com>
2018-03-02 10:13:58 +0700

    Project Member Fix

--- app/Http/Controllers/ProjectMemberController.php
+++ app/Http/Controllers/ProjectMemberController.php
@@ -7,6 +7,7 @@ use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Hash;
 
 use App\ProjectMember;
+use App\StudentsInfo;
 use App\Project;
 
 use JWTException;
@@ -22,45 +23,48 @@ class ProjectMemberController extends Controller
 				'id_students'				=> 'required',
 			]);
 
-			$collection = collect($request->input('id_students'));
-			$collection = str_replace('[','',$collection);
-			$collection = str_replace(']','',$collection);
-			$collection = str_replace('"','',$collection);
-			// $collection->toJson();
-			$collection = explode(',',$collection);
-			// $collection = implode(',',$collection);
-			// return $collection;
 
-			for($x=0;$x<=(count($collection)-1);$x++){
-				if(null==$collection[$x]){
-					break;
-				}else
-				{
-					if(ProjectMember::where('id_projects',$id)->where('id_students',$collection[$x])->count()==0){
+			// $collection = collect($request->input('id_students'));
+			// $collection = str_replace('[','',$collection);
+			// $collection = str_replace(']','',$collection);
+			// $collection = str_replace('"','',$collection);
+			// // $collection->toJson();
+			// $collection = explode(',',$collection);
+			// // $collection = implode(',',$collection);
+			// // return $collection;
+      //
+			// for($x=0;$x<=(count($collection)-1);$x++){
+			// 	if(null==$collection[$x]){
+			// 		break;
+			// 	}else
+			// 	{
+				$id_member = StudentsInfo::where('id_students',$request->id_students)->first()->id;
+					if(ProjectMember::where('id_projects',$id)->where('id_students',$request->id_students)->count()==0){
 
-					ProjectMember::Create([
-						'id_projects'		=>	$id,
-						'id_students'	 =>		$collection[$x]
-					]);
-				}else{
-					continue;
+					$memb = ProjectMember::Create([
+											'id_projects'		=>	$id,
+											'id_students'	 =>		$id_member,
+										]);
+
+					if($memb){
+						$response = [
+							'msg' => 'member Added',
+						];
+					}else{
+						$reponse = [
+							'msg' => 'member doesnt added',
+						];
+					}
 				}
 					// echo $collection[0];
 					// $data = str_replace('[','',$collection[0]);
 					// $data = str_replace(']','',$collection[$x]);
 					// echo $data;
 					// echo $collection[$x];
+					return response()->json($response,200);
 				}
-			}
-
-			$response = [
-										'msg' => 'Project Created',
-										'href' => "/v1/project/$id",
-										'method' => 'GET',
-									];
 				// return $
-			return response()->json($response,200);
-		}
+
 
 		public function destroy($id,$member){
 			ProjectMember::where('id_projects',$id)
